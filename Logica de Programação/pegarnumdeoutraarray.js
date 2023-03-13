4b30b499acf4155b72f494ccbae2fd316bcd98e3
isadoraeidt <125307953+isadoraeidt@users.noreply.github.com>
2023-03-13 14:15:57 -0300

    Add files via upload

---
+++ Logica de Programação/pegarnumdeoutraarray.js
@@ -0,0 +1,7 @@
+var valores1 = ['oi', 4, 9];
+console.log(valores1)
+var valores2 =[]
+var ultimo = valores1[valores1.length - 1];
+valores2.push(valores1[0])
+valores2.push(ultimo)
+console.log(valores2)
